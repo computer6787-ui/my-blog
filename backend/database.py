d67e64d84e1a88de18aff530a47a4cbb0d3eac0f
computer6787-ui <computer9669@outlook.com>
2026-07-08 14:07:19 +0600

    Update edit blog functionality

--- backend/database.py
+++ backend/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+from dotenv import load_dotenv
 
 DB_URL=os.getenv("DATABASE_URL")
 engine=create_engine(DB_URL)
