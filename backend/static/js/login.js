710506510615d25771eb90e8b72a1a4663aa3613
computer6787-ui <computer9669@outlook.com>
2026-08-18 11:33:53 +0600

    Added more features

--- backend/static/js/login.js
+++ backend/static/js/login.js
@@ -65,7 +65,7 @@ document.addEventListener('DOMContentLoaded', function() {
             event.preventDefault();
             const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
             passwordInput.setAttribute('type', type);
-            this.textContent = type === 'password' ? '👁️' : '👁️‍🗨️';
+            this.textContent = type === 'password' ? '🙈' : '🐵';
         });
     }
 });  
